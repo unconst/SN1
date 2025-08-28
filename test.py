1bc66af8d60bc7a4b7d1dbf0a2f05053df3f6df6
unconst <jake@bittensor.com>
2025-08-28 17:27:55 -0500

    keep base agent

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 from sn1 import Container
-with Container() as s:
+with Container( "agents/base_agent.py" ) as s:
     print(s.llm(prompt = "what is the capital of texas?" ))   # ->  query chutes using the key on the host.
