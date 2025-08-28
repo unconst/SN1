245676fb2fdf05c11a0bb693916846d176d00226
unconst <jake@bittensor.com>
2025-08-28 13:19:31 -0500

    show anything example

--- gen.py
+++ gen.py
@@ -1,16 +1,18 @@
 import sn1
 from sn1.boot import entrypoint
 
-@entrypoint()   # optional; without it, raw def name also works
+@entrypoint()
+def anything( z:str, y: int = 1 ) -> str:
+    return y * z
+
+@entrypoint()
 def in_func( x: int = 1 ):
-    # call host tool; host returns x+1
     return sn1.tools.out_func(x=x)
 
-@entrypoint()   # optional; without it, raw def name also works
+@entrypoint()
 def llm( prompt:str ):
-    # call host tool; host returns x+1
     return sn1.tools.llm( prompt = prompt )
 
-@entrypoint()   # optional; without it, raw def name also works
+@entrypoint() 
 def search( search:str ):
     return sn1.tools.search( search = search )
